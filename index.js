d38c6dc999ebf4828314ec06c6f0f244c1427b45
krishnaswaroop616 <kswaroop311@gmail.com>
2025-03-31 11:54:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,11 +28,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 
 
